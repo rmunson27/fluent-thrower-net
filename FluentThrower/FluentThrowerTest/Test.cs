d0476c0bb35cd677a122552593e2d259ecef9db0
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-07-19 20:44:38 -0400

    Added a basic test class
    There is more work to do as far as in-depth testing of every single method, but this test is enough to ensure the library works as expected on a basic level.

---
+++ FluentThrower/FluentThrowerTest/Test.cs
@@ -0,0 +1,122 @@
+﻿using Rem.Core.ComponentModel;
+using Rem.Core.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.CoreTest.Utilities;
+
+/// <summary>
+/// A basic test that the library works.
+/// </summary>
+[TestClass]
+public class Test
+{
+    #region NumericRange
+    /// <summary>
+    /// Tests the extension methods available to the <see cref="INumericRangeFluentThrower"/> interface implemented
+    /// by the <see cref="FluentThrower"/> class.
+    /// </summary>
+    [TestMethod]
+    public void TestNumericRange()
+    {
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => Throw.IfArgNegative(-3, "intArg"));
+        Throw.IfArgNegative(0, "intArg"); // Should not throw
+        Assert.ThrowsException<PropertySetOutOfRangeException>(() => Throw.IfPropSetNegative(-3, "IntProp"));
+        Throw.IfPropSetNegative(2, "IntProp"); // Should not throw
+    }
+    #endregion
+
+    #region DefaultOrUnnamed
+    /// <summary>
+    /// Tests the <see langword="enum"/> extension methods available to the
+    /// <see cref="IDefaultOrUnnamedFluentThrower"/> interface implemented by the <see cref="FluentThrower"/> class.
+    /// </summary>
+    [TestMethod, TestCategory(nameof(IDefaultOrUnnamedFluentThrower))]
+    public void TestDefaultOrUnnamed_Enums()
+    {
+        Assert.ThrowsException<InvalidEnumArgumentException>(() => Throw.IfEnumArgUnnamed(UnnamedE, "enumVal"));
+        Throw.IfEnumArgUnnamed(E.Value0, "enumVal"); // Should not throw
+        Assert.ThrowsException<InvalidEnumPropertySetException>(() => Throw.IfEnumPropSetUnnamed(UnnamedE, "EnumVal"));
+        Throw.IfEnumPropSetUnnamed(E.Value0, "EnumVal"); // Should not throw
+    }
+
+    /// <summary>
+    /// Tests the <see langword="struct"/> extension methods available to the
+    /// <see cref="IDefaultOrUnnamedFluentThrower"/> interface implemented by the <see cref="FluentThrower"/> class.
+    /// </summary>
+    [TestMethod, TestCategory(nameof(IDefaultOrUnnamedFluentThrower))]
+    public void TestDefaultOrUnnamed_Structs()
+    {
+        // General IDefaultableStruct implementations
+        Assert.ThrowsException<StructArgumentDefaultException>(() => Throw.IfStructArgDefault(default(S), "structVal"));
+        Throw.IfStructArgDefault(new S(new()), "structVal"); // Should not throw
+        Assert.ThrowsException<StructPropertySetDefaultException>(
+            () => Throw.IfStructPropSetDefault(default(S), "StructVal"));
+        Throw.IfStructPropSetDefault(new S(new()), "StructVal"); // Should not throw
+
+        // ImmutableArray
+        Assert.ThrowsException<StructArgumentDefaultException>(
+            () => Throw.IfStructArgDefault(default(ImmutableArray<int>), "arr"));
+        Throw.IfStructArgDefault(ImmutableArray<int>.Empty, "arr"); // Should not throw
+        Assert.ThrowsException<StructPropertySetDefaultException>(
+            () => Throw.IfStructPropSetDefault(default(ImmutableArray<int>), "Array"));
+        Throw.IfStructPropSetDefault(ImmutableArray<int>.Empty, "Array");
+    }
+
+    /// <summary>
+    /// Tests the nullability extension methods available to the <see cref="IDefaultOrUnnamedFluentThrower"/> interface
+    /// implemented by the <see cref="FluentThrower"/> class.
+    /// </summary>
+    [TestMethod, TestCategory(nameof(IDefaultOrUnnamedFluentThrower))]
+    public void TestDefaultOrUnnamed_Null()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Throw.IfArgNull(default(Version), "version"));
+        Throw.IfArgNull(new object(), "obj"); // Should not throw
+        Assert.ThrowsException<ArgumentNullException>(() => Throw.IfArgNull(default(int?), "nullableInt"));
+        Throw.IfArgNull(new int?(4), "nullableInt"); // Should not throw
+
+        Assert.ThrowsException<PropertySetNullException>(() => Throw.IfPropSetNull(default(Version), "Version"));
+        Throw.IfPropSetNull(new object(), "Object"); // Should not throw
+        Assert.ThrowsException<PropertySetNullException>(() => Throw.IfPropSetNull(default(int?), "nullableInt"));
+        Throw.IfPropSetNull(new int?(4), "nullableInt"); // Should not throw
+    }
+    #endregion
+
+    #region Comparison
+    /// <summary>
+    /// Tests the extension methods available to the <see cref="IComparisonFluentThrower"/> interface implemented
+    /// by the <see cref="FluentThrower"/> class.
+    /// </summary>
+    [TestMethod]
+    public void TestComparison()
+    {
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => Throw.IfArgGreaterThan(1, 2, "arg"));
+        Throw.IfArgGreaterThan(1, 1, "arg"); // Should not throw
+        Assert.ThrowsException<PropertySetOutOfRangeException>(() => Throw.IfPropSetGreaterThan(1, 2, "Prop"));
+        Throw.IfPropSetGreaterThan(1, 1, "Prop"); // Should not throw
+    }
+    #endregion
+
+    #region Helpers
+    private const E UnnamedE = (E)1;
+
+    private enum E
+    {
+        Value0
+    }
+
+    private readonly struct S : IDefaultableStruct
+    {
+        public bool IsDefault => O is null;
+
+        public object O { get; }
+
+        public S(object O) { this.O = O; }
+    }
+    #endregion
+}
