2683ca4e945ba2d98d54cfa28753ff5e967463db
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-07-19 20:32:47 -0400

    Added test project to the solution

---
+++ FluentThrower/FluentThrowerTest/global-usings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// Enable the fluent thrower globally
+global using Rem.Core.Utilities.FluentThrowerExtensions;
+global using static Rem.Core.Utilities.FluentThrowerInstanceWrapper;
+
